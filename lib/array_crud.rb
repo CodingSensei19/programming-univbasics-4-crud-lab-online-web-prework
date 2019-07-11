2640af1c60a0e55f64b7e739e25e2618d2638ab1
Aaron Wilson <codingsensei19@gmail.com>
2019-07-11 21:38:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,16 +26,16 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  
+end
+
+def remove_element_from_start_of_array(array)
   >> x = [4,5,6]
   => [4, 5, 6]
   >> x.shift
   => 4
   >> x
   => [5, 6]
-end
-
-def remove_element_from_start_of_array(array)
-
 end
 
 def retrieve_element_from_index(array, index_number)
